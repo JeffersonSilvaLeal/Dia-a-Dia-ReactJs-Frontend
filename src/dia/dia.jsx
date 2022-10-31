2baa976e1b6f4f8d17661491d5d2dc2f77bbcc17
Jefferson <jefferson.leal@7comm.com.br>
2022-10-31 09:44:59 -0300

    Componente Dia e About

---
+++ src/dia/dia.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Dia extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Dia</h1>
+            </div>
+        )
+    }
+}
