1e12a91871288652b938fc541d8e612fd1cc1581
Jefferson <jefferson.leal@7comm.com.br>
2022-10-31 10:35:52 -0300

    Componente PageHeader

---
+++ src/main/routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+import Dia from '../dia/dia'
+import About from '../about/about'
+
+export default props => (
+    <Router history={hashHistory}>
+        <Route path='/dias' component={Dia} />
+        <Route path='/about' component={About} />
+        <Redirect from='*' to='/Dias' />
+    </Router>
+)
