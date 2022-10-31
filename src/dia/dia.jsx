c7dd84f7ccbea8b8b2c4e73ff84136daac8486a6
Jefferson <jefferson.leal@7comm.com.br>
2022-10-31 14:26:46 -0300

    Evento Onchange

--- src/dia/dia.jsx
+++ src/dia/dia.jsx
@@ -6,18 +6,27 @@ import DiaList from './diaList'
 export default class Dia extends Component {
     constructor(props) {
         super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
 
+    handleChange(e) {
+        this.setState({...this.setState, description: e.target.value })
+    }
+
     handleAdd() {
-        console.log(this)
+        console.log(this.state.description)
     }
 
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <DiaForm handleAdd={this.handleAdd} />
+                <DiaForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
                 <DiaList />
             </div>
         )
