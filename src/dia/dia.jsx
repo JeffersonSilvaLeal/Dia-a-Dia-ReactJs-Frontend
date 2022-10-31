7aca5b527c219faec4e11394babab3d8003b3f61
Jefferson <jefferson.leal@7comm.com.br>
2022-10-31 11:42:38 -0300

    Componentes DiaForm e DiaList

--- src/dia/dia.jsx
+++ src/dia/dia.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import DiaForm from './diaForm'
+import DiaList from './diaList'
 
 export default class Dia extends Component {
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <DiaForm />
+                <DiaList />
             </div>
         )
     }
